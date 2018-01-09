785a062600c248831ccc1552be25acce18e99c48
James Moore <james@knowthen.com>
2018-01-09 11:14:57 -0800

    app function

--- counter/src/index.js
+++ counter/src/index.js
@@ -5,13 +5,13 @@ const { div, button } = hh(h);
 
 const initModel = 0;
 
-function view(model) {
+function view(dispatch, model) {
   return div([
     div({ className: 'mv2' }, `Count: ${model}`),
     button({ className: 'pv1 ph2 mr2', 
-      onclick: () => console.log('+ clicked!') }, '+'),
+      onclick: () => dispatch('plus') }, '+'),
     button({ className: 'pv1 ph2',
-      onclick: () => console.log('- clicked!') }, '-'),
+      onclick: () => dispatch('minus') }, '-'),
   ]);
 }
 
@@ -26,6 +26,22 @@ function update(msg, model) {
   }
 }
 
+// impure code below
+
+function app(initModel, update, view, node) {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+  node.appendChild(currentView);
+  function dispatch(msg){
+    model = update(msg, model);
+    const updatedView = view(dispatch, model);
+    node.replaceChild(updatedView, currentView);
+    currentView = updatedView;
+  }
+} 
+
 const rootNode = document.getElementById('app');
 
-rootNode.appendChild(view(update('minus', initModel)));
+app(initModel, update, view, rootNode);
+
+// rootNode.appendChild(view(update('minus', initModel)));
